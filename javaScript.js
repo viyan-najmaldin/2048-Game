a789737a2d95d13fb7102a03d06b29ecdc35f6d7
viyan-najmaldin <najmadinviyan@gmail.com>
2023-05-22 22:56:08 +0300

    count the score

--- javaScript.js
+++ javaScript.js
@@ -7,13 +7,15 @@ window.onload = function(){
     startGame();
 }
 
+
 function startGame(){
      arr=[
-        [4,8,4,4],
-        [4,4,4,0],
-        [4,0,0,0],
-        [8,0,0,0]
+        [0,0,0,0],
+        [0,0,0,0],
+        [4,4,2,2],
+        [4,4,2,2]
     ]
+
 
     for(let i=0 ;i<rows;i++){
         for(let j=0; j<columns; j++){
@@ -47,7 +49,6 @@ function updateElement(element,num){
     element.classList.add('smooth')
     element.classList.add('unsmooth')
     }
-    
 
 }
 function filtterZero(row){
@@ -64,7 +65,7 @@ function moveLeft(){
             if(arr[i][j]!=0){
                 let z=j
 
-                while(j>0 && arr[i][z-1]==0){
+                while(z>0 && arr[i][z-1]==0){
                     arr[i][z-1]=arr[i][z]
                     arr[i][z]=0
                      z--
@@ -72,6 +73,7 @@ function moveLeft(){
 
                 if(z>0  && arr[i][z]==arr[i][z-1]){
                     arr[i][z-1]*= 2
+                    score += arr[i][z-1]
                     arr[i][z]=0
                 }
             }
@@ -100,124 +102,111 @@ function moveLeft(){
 
 
 
-// function moveRight(){
-//     for(let i=0 ;i<rows;i++){
-//      let row= arr[i]   
-//     row= filtterZero(row)  
+function moveRight(){
+    for(let i=0 ;i<rows;i++){
+     let row= arr[i]   
+    row= filtterZero(row)  
 
-//      for(let x=row.length-1 ;x>=0;x--){  
-//       if(row[x] == row[x+1]){
-//         row[x+1] *= 2;
-//         row[x]=0;
-//        }   
-//      }
+     for(let x=row.length-1 ;x>=0;x--){  
+      if(row[x] == row[x+1]){
+        row[x+1] *= 2;
+        score += row[x+1]
+        row[x]=0;
+       }   
+     }
 
-//     row= filtterZero(row)     
-//     while(row.length<4) {
-//           row.unshift(0)}
+    row= filtterZero(row)     
+    while(row.length<4) {
+          row.unshift(0)}
     
-//    arr[i]=row
-//     } 
-// }
+   arr[i]=row
+    } 
+}
 
-// function moveUp(){
-//     for(let i=0 ;i<arr.length;i++){
-//         let y=[arr[0][i],arr[1][i],arr[2][i],arr[3][i]] 
-//         y = filtterZero(y) 
+function moveUp(){
+    for(let i=0 ;i<arr.length;i++){
+        let y=[arr[0][i],arr[1][i],arr[2][i],arr[3][i]] 
+        y = filtterZero(y) 
 
 
-//         for (let x = 0; x < y.length-1; x++){
-//             if (y[x] == y[x+1]) {
-//                 y[x] *= 2;
-//                 y[x+1] = 0;
+        for (let x = 0; x < y.length-1; x++){
+            if (y[x] == y[x+1]) {
+                y[x] *= 2;
+        score += y[x]
+
+                y[x+1] = 0;
+            }
+        }
+
+      
+          y= filtterZero(y)     
+          while(y.length<4) {
+                y.push(0)}         
+          
+        arr[0][i]= y[0]
+        arr[1][i]= y[1]
+        arr[2][i]= y[2]
+        arr[3][i]= y[3]
+
+    }
+}
+
+function moveDown(){
+    for(let i=0 ;i<arr.length;i++){
+        let y=[arr[0][i],arr[1][i],arr[2][i],arr[3][i]] 
+        y = filtterZero(y) 
+
+        for (let x=y.length-1 ;x>=0;x--){  // 4    
+            if (y[x] == y[x+1]){
+              y[x] *= 2;
+        score += y[x]
+              y[x+1]= 0;
+             }   
+           }
+      
+          y= filtterZero(y)     
+          while(y.length<4) {
+                y.unshift(0)}         
+          
+        arr[0][i]= y[0]
+        arr[1][i]= y[1]
+        arr[2][i]= y[2]
+        arr[3][i]= y[3]
+
+
+    }
+}
+
+
+
+
+
+////////////////////////////////////////
+
+
+// function moveRight(){
+//     for(let i=0; i<4; i++){
+//         for(let j=2; j>=0; j--){
+//             if(arr[i][j]!=0){
+//                 let z=j
+
+
+//                 while(z<3 && arr[i][z+1]==0){
+//                     arr[i][z+1]=arr[i][z]
+//                     arr[i][z]=0
+//                      z++
+//                             }
+
+//                             if(z<3  && arr[i][z]==arr[i][z+1]){
+//                                 arr[i][z+1]*= 2
+//                                 arr[i][z]=0
+//                             }
+
+                
 //             }
 //         }
-
-      
-//           y= filtterZero(y)     
-//           while(y.length<4) {
-//                 y.push(0)}         
-          
-//         arr[0][i]= y[0]
-//         arr[1][i]= y[1]
-//         arr[2][i]= y[2]
-//         arr[3][i]= y[3]
-
 //     }
 // }
-
-// function moveDown(){
-//     for(let i=0 ;i<arr.length;i++){
-//         let y=[arr[0][i],arr[1][i],arr[2][i],arr[3][i]] 
-//         y = filtterZero(y) 
-
-//         for (let x=y.length-1 ;x>=0;x--){  // 4    
-//             if (y[x] == y[x+1]){
-//               y[x] *= 2;
-//               y[x+1]= 0;
-//              }   
-//            }
-      
-//           y= filtterZero(y)     
-//           while(y.length<4) {
-//                 y.unshift(0)}         
-          
-//         arr[0][i]= y[0]
-//         arr[1][i]= y[1]
-//         arr[2][i]= y[2]
-//         arr[3][i]= y[3]
-
-
-//     }
-// }
-
-
-
-
-
-//////////////////////////////////////////
-
-function moveRight(){
-    for(let i=0; i<4 ; i++){
-        for(let j=2; j>=0 ; j++){
-            if(arr[i][j] !=0){
-                let z=j;
-
-                while(z<3 && arr[i][z+1]==0){
-                    arr[i][z+1]=arr[i][z]
-                    arr[i][z]=0
-                    z++
-                }
-
-                if(z<3 && arr[i][z]== arr[i][z+1]){
-                    arr[i][z+1] *=2
-                    arr[i][z]= 0
-                }
-            }
-        }
-    }
-}
-
-function moveRight(){
-    for(let i=0; i<4; i++){
-        for(let j=2; j>=0; j--){
-            if(arr[i][j]!=0){
-                let z=j
-
-                while(j>3 && arr[i][z+1]==0){
-                    arr[i][z+1]=arr[i][z]
-                    arr[i][z]=0
-                     z++
-                            }
-
-                if(z>0  && arr[i][z]==arr[i][z+1]){
-                    arr[i][z+1]*= 2
-                    arr[i][z]=0
-                }
-            }
-        }
-    }
-}
 
 
 document.addEventListener('keyup' ,(event)=>{
@@ -251,6 +240,13 @@ if (event.code == 'ArrowDown'){
     setTwo()
     console.log(arr)
 }
+
+let scoreNum =document.querySelector('.score-num')
+//console.log(scoreNum)
+
+scoreNum.innerText=score
+//console.log('the score is'+score)
+
 for(let i=0 ;i<rows;i++){
     for(let j=0; j<columns; j++){
      let  ele= document.getElementById(i+'-'+j)
@@ -261,6 +257,8 @@ for(let i=0 ;i<rows;i++){
     }}
 
 })
+  
+
 
 
 
@@ -292,5 +290,7 @@ function setTwo(){
             // else return;
         // } }
 }
+
+
 
 
