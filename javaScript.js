7b0d86c2f3a8dd3ff47d5f55e0d836b0a05550c1
viyan-najmaldin <najmadinviyan@gmail.com>
2023-05-27 18:10:14 +0300

    touch Device

--- javaScript.js
+++ javaScript.js
@@ -9,19 +9,36 @@ let tryDown = true;
 
 window.onload = function(){
     startGame();
+  
+    
+    
 }
 
 
+
 function startGame(){
-     arr=[
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
+    //  arr=[
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0]
+    // ]
+
+    arr=[
+        [2,2,2,2],
+        [2,2,4,2],
+        [4,4,4,4],
+        [8,4,4,4]
     ]
 
+   
+    touchDevice()
 
-    for(let i=0 ;i<rows;i++){
+
+    let newGame= document.getElementById('new-game');
+    newGame.addEventListener("click",()=>{location.reload()})
+
+for(let i=0 ;i<rows;i++){
         for(let j=0; j<columns; j++){
             let element= document.createElement('div')
             // console.log(arr[i][j])
@@ -40,6 +57,7 @@ function startGame(){
     
    setTwo()
    setTwo()
+   
 }
 
 function updateElement(element,num){
@@ -56,6 +74,7 @@ function updateElement(element,num){
     
    
 }
+
 function filtterZero(row){
   return  row.filter(num => num != 0)
 }
@@ -64,49 +83,53 @@ function filtterZero(row){
 
 ///////////////////////////////////// move left
 
-function moveLeft(){
-    for(let i=0; i<4; i++){
-        for(let j=1; j<4; j++){
-            if(arr[i][j]!=0){
-                let z=j
-
-                while(z>0 && arr[i][z-1]==0){
-                    arr[i][z-1]=arr[i][z]
-                    arr[i][z]=0
-                     z--
-                            }
-
-                if(z>0  && arr[i][z]==arr[i][z-1]){
-                    tryleft =true
-                    arr[i][z-1]*= 2
-                    score += arr[i][z-1]
-                    arr[i][z]=0
-                }
-                tryleft =false
-
-            }
-        }
-    }
-}
-
-
-
 // function moveLeft(){
-//     for(let i=0 ;i<rows;i++){
-//      let row= arr[i]   // 0 2 0 2
-//     row= filtterZero(row)  //2 2
-//      for(let x=0 ;x<row.length-1;x++){  // 4    
-//       if(row[x] == row[x+1]){
-//         row[x] *= 2;
-//         row[x+1]=0;
-//             }   
-//      }
-//     row= filtterZero(row)     
-//     while(row.length<4) {
-//           row.push(0) }
-//    arr[i]=row
-//     } 
+//     for(let i=0; i<4; i++){
+//         for(let j=1; j<4; j++){
+//             if(arr[i][j]!=0){
+//                 let z=j
+
+//                 while(z>0 && arr[i][z-1]==0){
+//                     arr[i][z-1]=arr[i][z]
+//                     arr[i][z]=0
+//                      z--
+//                             }
+
+//                 if(z>0  && arr[i][z]==arr[i][z-1]){
+//                     tryleft =true
+//                     arr[i][z-1]*= 2
+//                     score += arr[i][z-1]
+//                     arr[i][z]=0
+//                 }
+//                 tryleft =false
+
+//             }
+//         }
+//     }
 // }
+
+
+
+function moveLeft(){
+    for(let i=0 ;i<rows;i++){
+     let row= arr[i]   // 0 2 0 2
+    row= filtterZero(row)  //2 2
+     for(let x=0 ;x<row.length-1;x++){  // 4    
+      if(row[x] == row[x+1]){
+         tryleft =true
+        row[x] *= 2;
+         score += row[x]
+        row[x+1]=0;
+            }  
+         tryleft =false
+ 
+     }
+    row= filtterZero(row)     
+    while(row.length<4) {
+          row.push(0) }
+   arr[i]=row
+    } 
+}
 
 
 
@@ -283,17 +306,44 @@ console.log(scoreNum)
 
 scoreNum.innerText=score
 //console.log('the score is'+score)
-
+GameOver()
+setTwo()
 for(let i=0 ;i<rows;i++){
     for(let j=0; j<columns; j++){
      let  ele= document.getElementById(i+'-'+j) 
      let num =arr[i][j]
-        updateElement(ele,num)  
+        updateElement(ele,num) 
+        
+        
     }}
 
-    setTwo()
-    GameOver()
     
+  // won game   
+   let YouWinBox= document.querySelector('.win-box')
+   let CancelButton= document.querySelector('.cancel')
+   
+   CancelButton.addEventListener("click",()=>{
+       YouWinBox.classList.add('none')
+   })
+    
+   let newe= document.querySelector('.new-games')
+   newe.addEventListener("click",()=>{
+   location.reload()
+})
+
+// lost Game
+let YouloseBox= document.querySelector('.lose-box')
+console.log(YouloseBox)
+
+let CancelLoseBtn= document.querySelector('.cancel-lose')
+CancelLoseBtn.addEventListener("click",()=>{
+         YouloseBox.classList.add('none')
+    })
+
+    let news= document.querySelector('.neww-games')
+   news.addEventListener("click",()=>{
+   location.reload()
+})
 })
 
 function setTwo() {
@@ -332,8 +382,9 @@ function hasEmptyTile() {
     return false;
 }
 
+
 let maxx=0;
-let minn=0;
+
 function GameOver(){
    // let game =true;
    
@@ -346,15 +397,18 @@ function GameOver(){
             }
 
             if(!hasEmptyTile() && !ContinueTheGame()){
-                alert('Game over')
-                window.reload()
+                let YouloseBox= document.querySelector('.lose-box')
+                YouloseBox.classList.remove('none')
+                // location.reload()
             }
         }
     }
   
-     if(maxx == 2048  )
-       alert('You Win')
-    
+     if(maxx == 2048  ){
+   let YouWinBox= document.querySelector('.win-box')
+       YouWinBox.classList.remove('none')
+      
+    }
 
 }
 
@@ -369,3 +423,157 @@ function ContinueTheGame(){
 
 
 
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+function touchDevice(){
+
+    let touchArea = document.getElementById("box");
+let output = document.getElementById("output");
+
+//Initial mouse X and Y positions are 0
+
+let mouseX,
+  initialX = 0;
+let mouseY,
+  initialY = 0;
+let isSwiped;
+
+//Events for touch and mouse
+let events = {
+  mouse: {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+  touch: {
+    down: "touchstart",
+    move: "touchmove",
+    up: "touchend",
+  },
+};
+
+let deviceType = "";
+
+//Detect touch device
+
+const isTouchDevice = () => {
+  try {
+    //We try to create TouchEvent (it would fail for desktops and throw error)
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+//Get left and top of touchArea
+let rectLeft = touchArea.getBoundingClientRect().left;
+let rectTop = touchArea.getBoundingClientRect().top;
+
+//Get Exact X and Y position of mouse/touch
+const getXY = (e) => {
+  mouseX = (!isTouchDevice() ? e.pageX : e.touches[0].pageX) - rectLeft;
+  mouseY = (!isTouchDevice() ? e.pageY : e.touches[0].pageY) - rectTop;
+};
+
+isTouchDevice();
+
+//Start Swipe
+touchArea.addEventListener(events[deviceType].down, (event) => {
+  isSwiped = true;
+  //Get X and Y Position
+  getXY(event);
+  initialX = mouseX;
+  initialY = mouseY;
+});
+
+//Mousemove / touchmove
+touchArea.addEventListener(events[deviceType].move, (event) => {
+  if (!isTouchDevice()) {
+    event.preventDefault();
+  }
+  if (isSwiped) {
+    getXY(event);
+    let diffX = mouseX - initialX;
+    let diffY = mouseY - initialY;
+    if (Math.abs(diffY) > Math.abs(diffX)) {
+      output.innerText = diffY > 0 ? moveDown() : moveUp();
+    } else {
+      output.innerText = diffX > 0 ? moveRight() :     moveLeft();
+      ;
+    }
+
+
+    let scoreNum =document.querySelector('.score-num')
+    console.log(scoreNum)
+    
+    scoreNum.innerText=score
+    //console.log('the score is'+score)
+    GameOver()
+    setTwo()
+    for(let i=0 ;i<rows;i++){
+        for(let j=0; j<columns; j++){
+         let  ele= document.getElementById(i+'-'+j) 
+         let num =arr[i][j]
+            updateElement(ele,num) 
+            
+            
+        }}
+    
+        
+      // won game   
+       let YouWinBox= document.querySelector('.win-box')
+       let CancelButton= document.querySelector('.cancel')
+       
+       CancelButton.addEventListener("click",()=>{
+           YouWinBox.classList.add('none')
+       })
+        
+       let newe= document.querySelector('.new-games')
+       newe.addEventListener("click",()=>{
+       location.reload()
+    })
+    
+    // lost Game
+    let YouloseBox= document.querySelector('.lose-box')
+    console.log(YouloseBox)
+    
+    let CancelLoseBtn= document.querySelector('.cancel-lose')
+    CancelLoseBtn.addEventListener("click",()=>{
+             YouloseBox.classList.add('none')
+        })
+    
+        let news= document.querySelector('.neww-games')
+       news.addEventListener("click",()=>{
+       location.reload()
+    })
+
+
+  }
+});
+
+//Stop Drawing
+touchArea.addEventListener(events[deviceType].up, () => {
+  isSwiped = false;
+});
+
+touchArea.addEventListener("mouseleave", () => {
+  isSwiped = false;
+});
+
+window.onload = () => {
+  isSwiped = false;
+};
+
+}
